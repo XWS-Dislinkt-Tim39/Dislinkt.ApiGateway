0230f6098055284ff2a7b285d86e4923e81eb6f2
aleksandra.mitro <sandramitro99@gmail.com>
2022-07-08 19:28:54 +0200

    docker compose, prometheus and grafana

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -73,6 +73,7 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMetricServer();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
